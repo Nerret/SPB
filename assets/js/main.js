36deb514f9fef6c9acc5e59b3f9db8ef84c2ef74
Ryuggu <32564197+Ryuggu@users.noreply.github.com>
2018-05-20 23:30:34 +0200

    newsletter almost done
    >added a text field
    >added a button
    >added js to the button

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("subscribe-button");
+button.addEventListener('click', function() {
+  if (button.getAttribute("text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("text-original");
+  } else {
+    button.setAttribute("text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("text-swap");
+  }
+}, false);
